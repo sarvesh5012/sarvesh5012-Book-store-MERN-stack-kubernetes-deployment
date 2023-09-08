dfff699bfcb2216cdc14d1755e22e68f8bc62437
sarvesh5012 <114138465+sarvesh5012@users.noreply.github.com>
2023-09-08 10:05:39 +0530

    Add files via upload

---
+++ bookstore-docker-sarvesh/backend/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router); // localhost:5000/books
+
+mongoose
+  .connect(
+    "mongodb+srv://sarveshwark0512:test123@mongocluster.lnhxfb4.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
